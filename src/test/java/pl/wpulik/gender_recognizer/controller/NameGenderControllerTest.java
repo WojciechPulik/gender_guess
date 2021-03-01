d826c6a1519a285358efcb9b83fdb4c85e59f932
WojciechPulik <59091416+WojciechPulik@users.noreply.github.com>
2021-03-01 20:57:01 +0100

    controller package covered with tests

---
+++ src/test/java/pl/wpulik/gender_recognizer/controller/NameGenderControllerTest.java
@@ -0,0 +1,59 @@
+package pl.wpulik.gender_recognizer.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class NameGenderControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void guessGenderTestVariantOneMale() throws Exception {
+		mvc.perform(post("/gender/guess").param("variant", "1").param("fullname", "Wojciech Anna Nowak"))
+				.andExpect(status().isFound()).andExpect(content().string("MALE"));
+	}
+	
+	@Test
+	public void guessGenderTestVariantOneFemale() throws Exception {
+		mvc.perform(post("/gender/guess").param("variant", "1").param("fullname", "Anna Wojciech Nowak"))
+				.andExpect(status().isFound()).andExpect(content().string("FEMALE"));
+	}
+	
+	@Test
+	public void guessGenderTestVariantOneInconclusive() throws Exception {
+		mvc.perform(post("/gender/guess").param("variant", "1").param("fullname", "incorrect value"))
+				.andExpect(status().isFound()).andExpect(content().string("INCONCLUSIVE"));
+	}
+
+	@Test
+	public void guessGenderTestVariantTwoMale() throws Exception {
+		mvc.perform(post("/gender/guess").param("variant", "2").param("fullname", "Wojciech Kazimierz Anna Nowak"))
+				.andExpect(status().isFound()).andExpect(content().string("MALE"));
+	}
+	
+	@Test
+	public void guessGenderTestVariantTwoFemale() throws Exception {
+		mvc.perform(post("/gender/guess").param("variant", "2").param("fullname", "Helena Anna Nowak"))
+				.andExpect(status().isFound()).andExpect(content().string("FEMALE"));
+	}
+	
+	@Test
+	public void guessGenderTestVariantTwoInconclusive() throws Exception {
+		mvc.perform(post("/gender/guess").param("variant", "2").param("fullname", "Kazimierz Anna Nowak"))
+				.andExpect(status().isFound()).andExpect(content().string("INCONCLUSIVE"));
+	}
+
+}
